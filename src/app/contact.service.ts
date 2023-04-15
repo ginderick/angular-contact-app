2b4f7a3d2e3d6966e6242a0d9d1bf39e4c1c8106
Gin Derick <ginderick@pop-os.localdomain>
2023-04-15 19:08:52 +0800

    integrate firebase in contact service

--- src/app/contact.service.ts
+++ src/app/contact.service.ts
@@ -1,19 +1,33 @@
 import { Injectable } from '@angular/core';
 import { Contact } from './contacts';
-import { HttpClient } from '@angular/common/http';
+
+import { Observable } from 'rxjs';
+import {
+  collectionData,
+  Firestore,
+  DocumentData,
+  collection,
+  CollectionReference,
+} from '@angular/fire/firestore';
 
 @Injectable({
   providedIn: 'root',
 })
 export class ContactService {
   contacts: Contact[] = [];
-  constructor(private http: HttpClient) {}
+  private contactsCollection: CollectionReference<DocumentData>;
+
+  constructor(private firestore: Firestore) {
+    this.contactsCollection = collection(this.firestore, 'contacts');
+  }
 
   addToContact(contact: Contact) {
     this.contacts.push(contact);
   }
 
   getContacts() {
-    return this.http.get<Contact[]>('/assets/contact.json');
+    return collectionData(this.contactsCollection, {
+      idField: 'id',
+    }) as Observable<Contact[]>;
   }
 }
