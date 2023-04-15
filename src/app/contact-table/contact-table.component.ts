d99d92a8a589290d391f71eee06409609efb8b05
Gin Derick <ginderick@pop-os.localdomain>
2023-04-15 15:02:48 +0800

    update contact table to use ngOnChanges when table data is updated

--- src/app/contact-table/contact-table.component.ts
+++ src/app/contact-table/contact-table.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnChanges, SimpleChanges } from '@angular/core';
 import { MatTableDataSource } from '@angular/material/table';
 import { ContactService } from '../contact.service';
 import { Contact } from '../contacts';
@@ -9,19 +9,36 @@ import { Observable } from 'rxjs';
   templateUrl: './contact-table.component.html',
   styleUrls: ['./contact-table.component.css'],
 })
-export class ContactTableComponent implements OnInit {
-  contacts!: Observable<Contact[]>;
-  dataSource = new MatTableDataSource<any>();
+export class ContactTableComponent implements OnChanges {
+  // contacts!: Observable<Contact[]>;
+  @Input() contacts: Contact[] = [];
+
+  dataSource = new MatTableDataSource<Contact>();
+
   displayedColumns: string[] = ['id', 'name', 'email', 'contact', 'actions'];
 
-  constructor(private contactService: ContactService) {}
+  constructor() {}
 
-  ngOnInit() {
-    this.contacts = this.contactService.getContacts();
-    this.contacts.subscribe((data) => {
-      this.dataSource.data = data;
-    });
+  ngOnChanges(changes: SimpleChanges) {
+    console.log('ng OnChanges');
+    console.log(changes['contacts']);
+    if (changes['contacts'] && changes['contacts'].currentValue) {
+      console.log('If statement');
+      console.log(this.contacts);
+      // this.dataSource.data = [
+      //   ...this.dataSource.data,
+      //   ...changes.contacts.currentValue,
+      // ];
+      this.dataSource.data = changes['contacts'].currentValue;
+    }
   }
+
+  // ngOnInit() {
+  //   this.contacts = this.contactService.getContacts();
+  //   this.contacts.subscribe((data) => {
+  //     this.dataSource.data = data;
+  //   });
+  // }
 
   doSomething() {
     console.log('Hello');
