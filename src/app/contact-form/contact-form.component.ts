3bd0a05315eb87d51b817b8dabd2d8eef1f57e44
Gin Derick <ginderick@pop-os.localdomain>
2023-04-15 10:40:10 +0800

    add form buttons and labels

--- src/app/contact-form/contact-form.component.ts
+++ src/app/contact-form/contact-form.component.ts
@@ -1,8 +1,20 @@
 import { Component } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-contact-form',
   templateUrl: './contact-form.component.html',
   styleUrls: ['./contact-form.component.css'],
 })
-export class ContactFormComponent {}
+export class ContactFormComponent {
+  contactForm = this.formBuilder.group({
+    name: '',
+    email: '',
+    contact: '',
+  });
+  constructor(private formBuilder: FormBuilder) {}
+
+  onSubmit(): void {
+    console.log('Form submitted');
+  }
+}
