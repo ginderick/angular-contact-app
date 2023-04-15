5238b8da4e4d8263410bbfcd44ab69e2e443f370
Gin Derick <ginderick@pop-os.localdomain>
2023-04-15 11:30:47 +0800

    update contact table component

--- src/app/contact-table/contact-table.component.ts
+++ src/app/contact-table/contact-table.component.ts
@@ -1,8 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { MatTableDataSource } from '@angular/material/table';
+import { ContactService } from '../contact.service';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-contact-table',
   templateUrl: './contact-table.component.html',
   styleUrls: ['./contact-table.component.css'],
 })
-export class ContactTableComponent {}
+export class ContactTableComponent implements OnInit {
+  contacts!: Observable<{ name: string; email: string; contact: string }[]>;
+  dataSource = new MatTableDataSource<any>();
+  displayedColumns: string[] = ['name', 'email', 'contact', 'actions'];
+
+  constructor(private contactService: ContactService) {}
+
+  ngOnInit() {
+    this.contacts = this.contactService.getContacts();
+    console.log(this.contacts);
+    this.contacts.subscribe((data) => {
+      this.dataSource.data = data;
+    });
+  }
+
+  doSomething() {
+    console.log('Hello');
+  }
+}
