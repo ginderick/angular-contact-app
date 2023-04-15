59e475577f3dda52e3a9f07f170b96fd8b7e30cd
Gin Derick <ginderick@pop-os.localdomain>
2023-04-15 15:09:06 +0800

    remove unused import and remove placeholder data

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,18 +1,12 @@
 import { Component, ViewChild } from '@angular/core';
 import { Contact } from '../contacts';
-import { ContactTableComponent } from '../contact-table/contact-table.component';
-
 @Component({
   selector: 'app-contact',
   templateUrl: './contact.component.html',
   styleUrls: ['./contact.component.css'],
 })
 export class ContactComponent {
-  contacts: Contact[] = [
-    { id: 1, name: 'Item 1', email: 'Description 1', contact: '09272792254' },
-    { id: 2, name: 'Item 2', email: 'Description 2', contact: '09272792254' },
-    { id: 3, name: 'Item 3', email: 'Description 3', contact: '09272792254' },
-  ];
+  contacts: Contact[] = [];
 
   addItem(contact: Contact) {
     this.contacts = [contact, ...this.contacts];
