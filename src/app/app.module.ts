27fb2dc1f3479de23d8d57a88d793fa74e6a3aa1
Gin Derick <ginderick@pop-os.localdomain>
2023-04-15 10:30:41 +0800

    rename contact-editor component to contact component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,15 +3,15 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ContactEditorComponent } from './contact-editor/contact-editor.component';
+import { ContactComponent } from './contact/contact.component';
 import { RouterModule } from '@angular/router';
 
 @NgModule({
-  declarations: [AppComponent, ContactEditorComponent],
+  declarations: [AppComponent, ContactComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    RouterModule.forRoot([{ path: '', component: ContactEditorComponent }]),
+    RouterModule.forRoot([{ path: '', component: ContactComponent }]),
   ],
   providers: [],
   bootstrap: [AppComponent],
--- src/app/contact-editor/contact-editor.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-contact-editor',
-  templateUrl: './contact-editor.component.html',
-  styleUrls: ['./contact-editor.component.css'],
-})
-export class ContactEditorComponent {}
