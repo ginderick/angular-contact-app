db25a6111ef818253ea2f78fd09f46b5cd8c0e8d
Gin Derick <ginderick@pop-os.localdomain>
2023-04-15 15:36:56 +0800

    inject contactService and add ngOnInit to display contacts on initial load

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,16 +1,24 @@
-import { Component, ViewChild } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { Contact } from '../contacts';
+import { ContactService } from '../contact.service';
+import { of } from 'rxjs';
 @Component({
   selector: 'app-contact',
   templateUrl: './contact.component.html',
   styleUrls: ['./contact.component.css'],
 })
-export class ContactComponent {
+export class ContactComponent implements OnInit {
   contacts: Contact[] = [];
+
+  constructor(private contactService: ContactService) {}
+
+  ngOnInit() {
+    this.contactService.getContacts().subscribe((data) => {
+      this.contacts = [...data, ...this.contacts];
+    });
+  }
 
   addItem(contact: Contact) {
     this.contacts = [contact, ...this.contacts];
-    console.log('Adding contact');
-    console.log(this.contacts);
   }
 }
