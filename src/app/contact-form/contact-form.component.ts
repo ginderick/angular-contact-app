a5d222fa867cc357b65d8d95a95bbf05ee5bd381
Gin Derick <ginderick@pop-os.localdomain>
2023-04-15 12:24:57 +0800

    put form builder on the ngOnInit

--- src/app/contact-form/contact-form.component.ts
+++ src/app/contact-form/contact-form.component.ts
@@ -1,20 +1,26 @@
-import { Component } from '@angular/core';
-import { FormBuilder } from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-contact-form',
   templateUrl: './contact-form.component.html',
   styleUrls: ['./contact-form.component.css'],
 })
-export class ContactFormComponent {
-  contactForm = this.formBuilder.group({
-    name: '',
-    email: '',
-    contact: '',
-  });
+export class ContactFormComponent implements OnInit {
+  contactForm!: FormGroup;
+
   constructor(private formBuilder: FormBuilder) {}
+
+  ngOnInit(): void {
+    this.contactForm = this.formBuilder.group({
+      name: '',
+      email: '',
+      contact: '',
+    });
+  }
 
   onSubmit(): void {
     console.log('Form submitted');
+    console.log(this.contactForm.value);
   }
 }
